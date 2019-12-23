b802bb4d41a79a956b666c52e36c9ba4554fe6e8
cainy-a <cainatkinson@gmail.com>
2019-12-23 22:55:11 +0000

    Initial Commit

---
+++ NetherConverter/Form1.Designer.cs
@@ -0,0 +1,308 @@
+﻿namespace NetherConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.OverworldXtext = new System.Windows.Forms.TextBox();
+            this.NetherXtext = new System.Windows.Forms.TextBox();
+            this.OverworldYtext = new System.Windows.Forms.TextBox();
+            this.NetherYtext = new System.Windows.Forms.TextBox();
+            this.OverworldZtext = new System.Windows.Forms.TextBox();
+            this.NetherZtext = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.OverworldXout = new System.Windows.Forms.TextBox();
+            this.OverworldYout = new System.Windows.Forms.TextBox();
+            this.OverworldZout = new System.Windows.Forms.TextBox();
+            this.NetherXout = new System.Windows.Forms.TextBox();
+            this.NetherYout = new System.Windows.Forms.TextBox();
+            this.NetherZout = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Overworld";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Nether";
+            // 
+            // OverworldXtext
+            // 
+            this.OverworldXtext.Location = new System.Drawing.Point(74, 25);
+            this.OverworldXtext.Name = "OverworldXtext";
+            this.OverworldXtext.Size = new System.Drawing.Size(69, 20);
+            this.OverworldXtext.TabIndex = 1;
+            this.OverworldXtext.Text = "0";
+            this.OverworldXtext.TextChanged += new System.EventHandler(this.OverworldXtext_TextChanged);
+            // 
+            // NetherXtext
+            // 
+            this.NetherXtext.Location = new System.Drawing.Point(74, 48);
+            this.NetherXtext.Name = "NetherXtext";
+            this.NetherXtext.Size = new System.Drawing.Size(69, 20);
+            this.NetherXtext.TabIndex = 1;
+            this.NetherXtext.Text = "0";
+            this.NetherXtext.TextChanged += new System.EventHandler(this.NetherXtext_TextChanged);
+            // 
+            // OverworldYtext
+            // 
+            this.OverworldYtext.Location = new System.Drawing.Point(149, 25);
+            this.OverworldYtext.Name = "OverworldYtext";
+            this.OverworldYtext.Size = new System.Drawing.Size(69, 20);
+            this.OverworldYtext.TabIndex = 1;
+            this.OverworldYtext.Text = "0";
+            this.OverworldYtext.TextChanged += new System.EventHandler(this.OverworldYtext_TextChanged);
+            // 
+            // NetherYtext
+            // 
+            this.NetherYtext.Location = new System.Drawing.Point(149, 48);
+            this.NetherYtext.Name = "NetherYtext";
+            this.NetherYtext.Size = new System.Drawing.Size(69, 20);
+            this.NetherYtext.TabIndex = 1;
+            this.NetherYtext.Text = "0";
+            this.NetherYtext.TextChanged += new System.EventHandler(this.NetherYtext_TextChanged);
+            // 
+            // OverworldZtext
+            // 
+            this.OverworldZtext.Location = new System.Drawing.Point(224, 25);
+            this.OverworldZtext.Name = "OverworldZtext";
+            this.OverworldZtext.Size = new System.Drawing.Size(69, 20);
+            this.OverworldZtext.TabIndex = 1;
+            this.OverworldZtext.Text = "0";
+            this.OverworldZtext.TextChanged += new System.EventHandler(this.OverworldZtext_TextChanged);
+            // 
+            // NetherZtext
+            // 
+            this.NetherZtext.Location = new System.Drawing.Point(224, 48);
+            this.NetherZtext.Name = "NetherZtext";
+            this.NetherZtext.Size = new System.Drawing.Size(69, 20);
+            this.NetherZtext.TabIndex = 1;
+            this.NetherZtext.Text = "0";
+            this.NetherZtext.TextChanged += new System.EventHandler(this.NetherZtext_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(102, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "X";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(178, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(14, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Y";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(252, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(14, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Z";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 83);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Overworld";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(13, 106);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(39, 13);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Nether";
+            // 
+            // OverworldXout
+            // 
+            this.OverworldXout.Location = new System.Drawing.Point(74, 80);
+            this.OverworldXout.Name = "OverworldXout";
+            this.OverworldXout.ReadOnly = true;
+            this.OverworldXout.Size = new System.Drawing.Size(69, 20);
+            this.OverworldXout.TabIndex = 1;
+            this.OverworldXout.Text = "0";
+            this.OverworldXout.TextChanged += new System.EventHandler(this.OverworldXtext_TextChanged);
+            // 
+            // OverworldYout
+            // 
+            this.OverworldYout.Location = new System.Drawing.Point(149, 80);
+            this.OverworldYout.Name = "OverworldYout";
+            this.OverworldYout.ReadOnly = true;
+            this.OverworldYout.Size = new System.Drawing.Size(69, 20);
+            this.OverworldYout.TabIndex = 1;
+            this.OverworldYout.Text = "0";
+            this.OverworldYout.TextChanged += new System.EventHandler(this.OverworldYtext_TextChanged);
+            // 
+            // OverworldZout
+            // 
+            this.OverworldZout.Location = new System.Drawing.Point(224, 80);
+            this.OverworldZout.Name = "OverworldZout";
+            this.OverworldZout.ReadOnly = true;
+            this.OverworldZout.Size = new System.Drawing.Size(69, 20);
+            this.OverworldZout.TabIndex = 1;
+            this.OverworldZout.Text = "0";
+            this.OverworldZout.TextChanged += new System.EventHandler(this.OverworldZtext_TextChanged);
+            // 
+            // NetherXout
+            // 
+            this.NetherXout.Location = new System.Drawing.Point(74, 103);
+            this.NetherXout.Name = "NetherXout";
+            this.NetherXout.ReadOnly = true;
+            this.NetherXout.Size = new System.Drawing.Size(69, 20);
+            this.NetherXout.TabIndex = 1;
+            this.NetherXout.Text = "0";
+            this.NetherXout.TextChanged += new System.EventHandler(this.NetherXtext_TextChanged);
+            // 
+            // NetherYout
+            // 
+            this.NetherYout.Location = new System.Drawing.Point(149, 103);
+            this.NetherYout.Name = "NetherYout";
+            this.NetherYout.ReadOnly = true;
+            this.NetherYout.Size = new System.Drawing.Size(69, 20);
+            this.NetherYout.TabIndex = 1;
+            this.NetherYout.Text = "0";
+            this.NetherYout.TextChanged += new System.EventHandler(this.NetherYtext_TextChanged);
+            // 
+            // NetherZout
+            // 
+            this.NetherZout.Location = new System.Drawing.Point(224, 103);
+            this.NetherZout.Name = "NetherZout";
+            this.NetherZout.ReadOnly = true;
+            this.NetherZout.Size = new System.Drawing.Size(69, 20);
+            this.NetherZout.TabIndex = 1;
+            this.NetherZout.Text = "0";
+            this.NetherZout.TextChanged += new System.EventHandler(this.NetherZtext_TextChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(300, 38);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(16, 13);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "In";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(303, 93);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(24, 13);
+            this.label9.TabIndex = 4;
+            this.label9.Text = "Out";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 138);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.NetherZout);
+            this.Controls.Add(this.NetherZtext);
+            this.Controls.Add(this.NetherYout);
+            this.Controls.Add(this.NetherYtext);
+            this.Controls.Add(this.NetherXout);
+            this.Controls.Add(this.OverworldZout);
+            this.Controls.Add(this.NetherXtext);
+            this.Controls.Add(this.OverworldYout);
+            this.Controls.Add(this.OverworldZtext);
+            this.Controls.Add(this.OverworldXout);
+            this.Controls.Add(this.OverworldYtext);
+            this.Controls.Add(this.OverworldXtext);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Nether Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox OverworldXtext;
+        private System.Windows.Forms.TextBox NetherXtext;
+        private System.Windows.Forms.TextBox OverworldYtext;
+        private System.Windows.Forms.TextBox NetherYtext;
+        private System.Windows.Forms.TextBox OverworldZtext;
+        private System.Windows.Forms.TextBox NetherZtext;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox OverworldXout;
+        private System.Windows.Forms.TextBox OverworldYout;
+        private System.Windows.Forms.TextBox OverworldZout;
+        private System.Windows.Forms.TextBox NetherXout;
+        private System.Windows.Forms.TextBox NetherYout;
+        private System.Windows.Forms.TextBox NetherZout;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ NetherConverter/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NetherConverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void OverworldXtext_TextChanged(object sender, EventArgs e)
+        {
+            if (OverworldXtext.Text != "")
+            {
+                NetherXout.Text = (System.Convert.ToDouble(OverworldXtext.Text) / 8).ToString();
+            }
+        }
+
+        private void OverworldYtext_TextChanged(object sender, EventArgs e)
+        {
+            if (OverworldYtext.Text != "")
+            {
+                NetherYout.Text = System.Convert.ToDouble(OverworldYtext.Text).ToString();
+            }
+        }
+
+        private void OverworldZtext_TextChanged(object sender, EventArgs e)
+        {
+            if (OverworldZtext.Text != "")
+            {
+                NetherZout.Text = (System.Convert.ToDouble(OverworldZtext.Text) / 8).ToString();
+            }
+        }
+
+        private void NetherXtext_TextChanged(object sender, EventArgs e)
+        {
+            if (NetherXtext.Text != "")
+            {
+                OverworldXout.Text = (System.Convert.ToDouble(NetherXtext.Text) * 8).ToString();
+            }
+        }
+
+        private void NetherYtext_TextChanged(object sender, EventArgs e)
+        {
+            if (NetherYtext.Text != "")
+            {
+                OverworldYout.Text = System.Convert.ToDouble(NetherYtext.Text).ToString();
+            }
+        }
+
+        private void NetherZtext_TextChanged(object sender, EventArgs e)
+        {
+            if (NetherZtext.Text != "")
+            {
+                OverworldZout.Text = (System.Convert.ToDouble(NetherZtext.Text) * 8).ToString();
+            }
+        }
+    }
+}
